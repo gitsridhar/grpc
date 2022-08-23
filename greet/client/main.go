e2cdf1974ae8817533c74d49916dde93e7416cda
gitsridhar <svenkat@us.ibm.com>
2022-08-23 16:48:46 -0400

    Added SSL support.
    Signed-off-by: gitsridhar <svenkat@us.ibm.com>

--- greet/client/main.go
+++ greet/client/main.go
@@ -6,13 +6,28 @@ import (
 
 	pb "github.com/gitsridhar/grpc/greet/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	tls := true
+
+	opts := []grpc.DialOption{}
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificate: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+	conn, err := grpc.Dial(addr, opts...)
 
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
@@ -22,11 +37,11 @@ func main() {
 
 	c := pb.NewGreetServiceClient(conn)
 
-	//doGreet(c)
+	doGreet(c)
 
-	//doGreetManyTimes(c)
+	doGreetManyTimes(c)
 
-	//doLongGreet(c)
+	doLongGreet(c)
 
 	doGreetEveryone(c)
 
